ba64819bc07fc22a8b6b69d9e8215be1b0dfd391
danielniemiec44@wp.pl <danielniemiec44@gmail.com>
2020-02-29 12:37:11 +0100

    Czesc Daniela

--- src/com/xdaanielx/Main.java
+++ src/com/xdaanielx/Main.java
@@ -1,10 +1,18 @@
 package com.xdaanielx;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// Ta czescia kodu zajmuje sie Daniel
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj liczbe a: ");
+        double a = scanner.nextDouble();
+        System.out.print("Podaj liczbe b: ");
+        double b = scanner.nextDouble();
+        System.out.println("Suma liczb a i b wynosi: " + dodaj(a,b));
 
 
     }
